82689dea538f0f2f97dae40d22c89439aef1cf69
Bakhtiyor Sultanov <119063145+Bakhtiyor-Sultanov@users.noreply.github.com>
2026-09-14 00:29:41 +0800

    Update exercise3.py
    {eachperson:.2f} instead of rounding, takes 2 nums after "."

--- exercise3.py
+++ exercise3.py
@@ -1,6 +1,6 @@
 meal=int(input("Enter the meal price: "))
 tip=int(input("Enter the tip percentage: "))
 ppl=int(input("Enter the number of people: "))
-eachperson=meal+(meal*tip/100)/ppl
-print("Each person pays: "+str(round(eachperson,2))+" RMB")
+eachperson= (meal+meal*tip/100)/ppl
+print(f"Each person pays: {eachperson:.2f} RMB")
 
