86f5d92f5341bfcd7d6c79ea37ba63cdf6694799
Bakhtiyor Sultanov <119063145+Bakhtiyor-Sultanov@users.noreply.github.com>
2026-09-14 00:30:18 +0800

    Update exercise2.py
    Rounds the Avr

--- exercise2.py
+++ exercise2.py
@@ -3,6 +3,6 @@ second_grade=int(input("2nd grade (int between 0 and 100):"))
 third_grade=int(input("3srd grade (int between 0 and 100):"))
 
 if all(0 <= n <= 100 for n in (first_grade, second_grade, third_grade)):
-    print("Avarege grade: ", (first_grade + second_grade + third_grade)/3)
+    print("Avarege grade: ", round((first_grade + second_grade + third_grade)/3))
 else:
     print(ValueError)
