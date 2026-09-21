48e86434e982ed55e35591a5080a0d97b1c36037
Bakhtiyor Sultanov <119063145+Bakhtiyor-Sultanov@users.noreply.github.com>
2026-09-21 12:16:06 +0800

    Update exercise3.py

--- HW2/exercise3.py
+++ HW2/exercise3.py
@@ -3,11 +3,12 @@ leap=False
 wc=False
 eu=False
 print(f"Year:{year}")
-if year%4==0 or year%400==0:
-    leap=True
-if year%100==0:
-    leap=False
 
+if year % 400 == 0 or (year % 4 == 0 and year % 100 != 0):
+    leap = True
+else:
+    leap = False
+    
 if leap==True:
     print("Leap year")
 else:
