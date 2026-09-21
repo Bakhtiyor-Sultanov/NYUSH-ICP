65adf216f4e01ee8e8c1474e0d0143a05d4e6a21
Bakhtiyor Sultanov <119063145+Bakhtiyor-Sultanov@users.noreply.github.com>
2026-09-21 12:15:15 +0800

    Update exercise1.py

--- HW2/exercise1.py
+++ HW2/exercise1.py
@@ -1,14 +1,15 @@
-table=str(input("Is a table available? Yes/No?: "))
+table=str(input("Is a table available? (yes or no):"))
 table=table.upper()
-
+people=0
+if table=="NO":
+    print("Sorry, the restaurant is fully booked.")
 if table=="YES":
-    pass
-else:
-    print("No")
-people=int(input("How many people?: "))
-if people <= 6:
-    print("Reservation confirmed. A regular table has been reserved")
-elif people > 6:
-    print("Reservation confirmed. A large table has been reserved")
+    people=int(input("How many people?: "))
+    if people <= 6:
+        print("Reservation confirmed. A regular table has been reserved.")
+    elif people > 6:
+        print("Reservation confirmed. A large table has been reserved.")
+
+
 
     
